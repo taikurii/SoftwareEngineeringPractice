9f32e4733abb1a10bf8695bb0a6dd96ba3336068
Jolie Elins <jolieelins@Jolie-Bettes-MacBook.local>
2020-01-24 02:22:52 -0500

    isEmailValid work

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -8,47 +8,54 @@ public class BankAccount {
     /**
      * @throws IllegalArgumentException if email is invalid
      */
-    public BankAccount(String email, double startingBalance){
-        if (isEmailValid(email)){
+    public BankAccount(String email, double startingBalance) {
+        if (isEmailValid(email)) {
             this.email = email;
             this.balance = startingBalance;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
         }
     }
 
-    public double getBalance(){
+    public double getBalance() {
         return balance;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
-    public void withdraw (double amount)  {
+    public void withdraw(double amount) {
         balance -= amount;
 
     }
 
 
-    public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1 || email.indexOf('#') != -1 || ) {
+    public static boolean isEmailValid(String email) {
+        if (email.indexOf('@') == -1 || email.indexOf('#') != -1 || email.indexOf('!') != -1) {
             return false;
-        }
-        else if (email.indexOf('-') !){
+        } else if (email.charAt(email.indexOf('@') - 1) == '-' || email.charAt(email.indexOf('@') - 1) == '.' || email.charAt(email.indexOf('@') - 1) == '_') {
+            return false;
+        } else if (email.indexOf('-') != -1 || email.indexOf('.') != -1 || email.indexOf('_') != -1) {
             String x = "";
             String y = "";
-            for (int i = 0; i < email.length(); i++){
-                if ((x == "-" && y == "-") || (x == "." && y == ".") || (x == "_" && y == "_")){
+            for (int i = 0; i < email.length(); i++) {
+                if ((x == "-" && y == "-") || (x == "." && y == ".") || (x == "_" && y == "_")) {
+                    return false;
+                } else if ((x == "-" && y == "_") || (x == "-" && y == ".") || (x == "." && y == "-") || (x == "." && y == "_")) {
+                    return false;
+                } else if ((x == "_" && y == "-") || (x == "_" && y == ".")) {
                     return false;
                 }
             }
-
         }
+        else if(email.charAt(0) == '.' || email.charAt(0) == '_' || email.charAt(0) == '-') {
+            return false;
+        }
+
     }
 }
 
